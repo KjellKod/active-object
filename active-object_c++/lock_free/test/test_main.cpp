953ed966ef0065e24887a2d279e87ca441efce31
kjell <kjell@kjell-UX31E.(none)>
2013-02-03 19:41:04 +0100

    removed obsolete c++ active object. It is better to use the c++11 active object !

--- active-object_c++/lock_free/test/test_main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <gtest/gtest.h>
-
-int main(int argc, char **argv) {
-  std::cout << "Running main() from circularfifo test main.cpp\n";
-
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
